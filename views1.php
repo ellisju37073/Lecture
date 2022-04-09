00365dfb38666abcdc0ac4ed6547c55dc7d0c5d1
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2022-04-09 04:50:22 -0700

    update

--- views1.php
+++ views1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Views</title>
+    <title>Views is a great page</title>
 </head>
 <body>
     <h1>Creating Variables</h1>
